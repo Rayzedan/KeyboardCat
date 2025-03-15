9385e1d6a07b4a4646f4c09dba616d6b8c08db0a
d.bezlepkin <d.bezlepkin@prosoftsystems.ru>
2025-03-16 01:58:40 +0500

    add(ui): removed old files, added draft version

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## SDL3 App From Source Minimal Example
-This is a minimal example for building and using SDL3, SDL_Mixer, SDL_Image, and SDL_ttf_ from source 
+This is a minimal example for building and using SDL3, SDL_Mixer, SDL_Image, and SDL_ttf_ from source
 using C++ and CMake. It also demonstrates setting up things like macOS/iOS
 bundles.
-See [src/main.cpp](src/main.cpp) for the code. 
+See [src/main.cpp](src/main.cpp) for the code.
 
 ### Building And Running
 Are you a complete beginner? If so, read [this](https://github.com/Ravbug/sdl3-sample/wiki/Setting-up-your-computer)!
@@ -13,7 +13,7 @@ git clone https://github.com/Ravbug/sdl3-sample --depth=1 --recurse-submodules
 cd sdl3-sample
 cmake -S . -B build
 ```
-You can also use an init script inside [`config/`](config/). Then open the IDE project inside `build/` 
+You can also use an init script inside [`config/`](config/). Then open the IDE project inside `build/`
 (If you had CMake generate one) and run!
 
 ## Supported Platforms
@@ -39,9 +39,6 @@ Just update the submodule:
 cd SDL
 git pull
 cd ..
-
-cd SDL_ttf
-git pull
 ```
 You don't need to use a submodule, you can also copy the source in directly. This
 repository uses a submodule to keep its size to a minimum.
--- src/main.cpp
+++ src/main.cpp
@@ -1,195 +1,173 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3/SDL_properties.h>
 #include <SDL3_image/SDL_image.h>
-#include <cmath>
-#include <string_view>
 #include <filesystem>
+#include <fstream>
+#include <gif_lib.h>
+#include <iostream>
+#include <linux/input.h>
+#include <set>
+#include <string.h>
+#include <vector>
 
-constexpr uint32_t windowStartWidth = 400;
-constexpr uint32_t windowStartHeight = 400;
-
-struct AppContext {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Texture* messageTex, *imageTex;
-    SDL_FRect messageDest;
-    SDL_AudioDeviceID audioDevice;
-    Mix_Music* music;
-    SDL_AppResult app_quit = SDL_APP_CONTINUE;
-};
-
-SDL_AppResult SDL_Fail(){
+SDL_AppResult SDL_Fail()
+{
     SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
     return SDL_APP_FAILURE;
 }
 
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
-    // init the library, here we make a window so we only need the Video capabilities.
-    if (not SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)){
-        return SDL_Fail();
-    }
-    
-    // init TTF
-    if (not TTF_Init()) {
-        return SDL_Fail();
-    }
-    
-    // create a window
-   
-    SDL_Window* window = SDL_CreateWindow("SDL Minimal Sample", windowStartWidth, windowStartHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
-    if (not window){
-        return SDL_Fail();
-    }
-    
-    // create a renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
-    if (not renderer){
-        return SDL_Fail();
-    }
-    
-    // load the font
-#if __ANDROID__
-    std::filesystem::path basePath = "";   // on Android we do not want to use basepath. Instead, assets are available at the root directory.
-#else
-    auto basePathPtr = SDL_GetBasePath();
-     if (not basePathPtr){
-        return SDL_Fail();
-    }
-     const std::filesystem::path basePath = basePathPtr;
-#endif
+std::vector<SDL_Surface*> LoadGifFrames(const char* filePath)
+{
+    std::vector<SDL_Surface*> frames;
 
-    const auto fontPath = basePath / "Inter-VariableFont.ttf";
-    TTF_Font* font = TTF_OpenFont(fontPath.string().c_str(), 36);
-    if (not font) {
-        return SDL_Fail();
-    }
-
-    // render the font to a surface
-    const std::string_view text = "Hello SDL!";
-    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, text.data(), text.length(), { 255,255,255 });
-
-    // make a texture from the surface
-    SDL_Texture* messageTex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
-
-    // we no longer need the font or the surface, so we can destroy those now.
-    TTF_CloseFont(font);
-    SDL_DestroySurface(surfaceMessage);
-
-    // load the SVG
-    auto svg_surface = IMG_Load((basePath / "gs_tiger.svg").string().c_str());
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, svg_surface);
-    SDL_DestroySurface(svg_surface);
-    
-
-    // get the on-screen dimensions of the text. this is necessary for rendering it
-    auto messageTexProps = SDL_GetTextureProperties(messageTex);
-    SDL_FRect text_rect{
-            .x = 0,
-            .y = 0,
-            .w = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0)),
-            .h = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_HEIGHT_NUMBER, 0))
-    };
-
-    // init SDL Mixer
-    auto audioDevice = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
-    if (not audioDevice) {
-        return SDL_Fail();
-    }
-    if (not Mix_OpenAudio(audioDevice, NULL)) {
-        return SDL_Fail();
-    }
-
-    // load the music
-    auto musicPath = basePath / "the_entertainer.ogg";
-    auto music = Mix_LoadMUS(musicPath.string().c_str());
-    if (not music) {
-        return SDL_Fail();
-    }
-
-    // play the music (does not loop)
-    Mix_PlayMusic(music, 0);
-    
-    // print some information about the window
-    SDL_ShowWindow(window);
+    GifFileType* gifFile = DGifOpenFileName(filePath, nullptr);
+    if (!gifFile)
     {
-        int width, height, bbwidth, bbheight;
-        SDL_GetWindowSize(window, &width, &height);
-        SDL_GetWindowSizeInPixels(window, &bbwidth, &bbheight);
-        SDL_Log("Window size: %ix%i", width, height);
-        SDL_Log("Backbuffer size: %ix%i", bbwidth, bbheight);
-        if (width != bbwidth){
-            SDL_Log("This is a highdpi environment.");
+        std::cerr << "Failed to open GIF file: " << filePath << std::endl;
+        return frames;
+    }
+
+    if (DGifSlurp(gifFile) != GIF_OK)
+    {
+        DGifCloseFile(gifFile, nullptr);
+        return frames;
+    }
+
+    std::set<GifColorType> unique_colors;
+    for (int i = 0; i < gifFile->ImageCount; ++i)
+    {
+        SavedImage* frame = &gifFile->SavedImages[i];
+        ColorMapObject* colorMap =
+            frame->ImageDesc.ColorMap ? frame->ImageDesc.ColorMap : gifFile->SColorMap;
+
+        SDL_Surface* surface = SDL_CreateSurface(frame->ImageDesc.Width,
+            frame->ImageDesc.Height, SDL_PIXELFORMAT_ARGB8888);
+
+        if (!surface)
+        {
+            std::cerr << "Failed to create surface for frame " << i << std::endl;
+            continue;
         }
+        for (int y = 0; y < frame->ImageDesc.Height; ++y)
+        {
+            for (int x = 0; x < frame->ImageDesc.Width; ++x)
+            {
+                int pixelIndex = y * frame->ImageDesc.Width + x;
+                GifByteType colorIndex = frame->RasterBits[pixelIndex];
+                GifColorType color = colorMap->Colors[colorIndex];
+                Uint32 pixelColor;
+                if (color.Red >= 240 && color.Green >= 180 && color.Blue >= 180)
+                {
+                    pixelColor = 0x00000000;
+                }
+                else
+                {
+                    pixelColor =
+                        (255 << 24) | (color.Red << 16) | (color.Green << 8) | color.Blue;
+                }
+                Uint32* pixels = (Uint32*)surface->pixels;
+                pixels[pixelIndex] = pixelColor;
+            }
+        }
+
+        frames.push_back(surface);
     }
 
-    // set up the application data
-    *appstate = new AppContext{
-       .window = window,
-       .renderer = renderer,
-       .messageTex = messageTex,
-       .imageTex = tex,
-       .messageDest = text_rect,
-       .audioDevice = audioDevice,
-       .music = music,
-    };
-    
-    SDL_SetRenderVSync(renderer, -1);   // enable vysnc
-    
-    SDL_Log("Application started successfully!");
-
-    return SDL_APP_CONTINUE;
+    DGifCloseFile(gifFile, nullptr);
+    return frames;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
-    auto* app = (AppContext*)appstate;
-    
-    if (event->type == SDL_EVENT_QUIT) {
-        app->app_quit = SDL_APP_SUCCESS;
+
+int main(int argc, char** argv)
+{
+    struct input_event keyboardEvent;
+    std::ifstream file("/dev/input/event3");
+    char dataInput[sizeof(keyboardEvent)];
+    if (!file.is_open())
+    {
+        return -1;
     }
 
-    return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    auto* app = (AppContext*)appstate;
-
-    // draw a color
-    auto time = SDL_GetTicks() / 1000.f;
-    auto red = (std::sin(time) + 1) / 2.0 * 255;
-    auto green = (std::sin(time / 2) + 1) / 2.0 * 255;
-    auto blue = (std::sin(time) * 2 + 1) / 2.0 * 255;
-    
-    SDL_SetRenderDrawColor(app->renderer, red, green, blue, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(app->renderer);
-
-    // Renderer uses the painter's algorithm to make the text appear above the image, we must render the image first.
-    SDL_RenderTexture(app->renderer, app->imageTex, NULL, NULL);
-    SDL_RenderTexture(app->renderer, app->messageTex, NULL, &app->messageDest);
-
-    SDL_RenderPresent(app->renderer);
-
-    return app->app_quit;
-}
-
-void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-    auto* app = (AppContext*)appstate;
-    if (app) {
-        SDL_DestroyRenderer(app->renderer);
-        SDL_DestroyWindow(app->window);
-
-        Mix_FadeOutMusic(1000);  // prevent the music from abruptly ending.
-        Mix_FreeMusic(app->music); // this call blocks until the music has finished fading
-        Mix_CloseAudio();
-        SDL_CloseAudioDevice(app->audioDevice);
-
-        delete app;
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        return -1;
     }
-    TTF_Quit();
-    Mix_Quit();
 
-    SDL_Log("Application quit successfully!");
+    int windowWidth = 249;
+    int windowHeight = 153;
+
+    SDL_Window* window = SDL_CreateWindow("Bongo Cat", windowWidth, windowHeight,
+        SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_BORDERLESS | SDL_WINDOW_TRANSPARENT);
+    if (!window)
+    {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        return SDL_Fail();
+    }
+    SDL_SetWindowPosition(window, 1920 - windowWidth - 3, 1200 - windowHeight + 47);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer)
+    {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return SDL_Fail();
+    }
+    auto basePathPtr = SDL_GetBasePath();
+    if (basePathPtr == nullptr)
+    {
+        return SDL_Fail();
+    }
+    const std::filesystem::path basePath = basePathPtr;
+    const auto fontPath = basePath / "bongo_cat.gif";
+    auto frames = LoadGifFrames(fontPath.string().c_str());
+    if (frames.empty())
+    {
+        std::cerr << "Failed to load GIF: " << SDL_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return SDL_Fail();
+    }
+
+    bool running = true;
+    size_t currentFrame = 0;
+    while (running)
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+
+        SDL_Texture* texture =
+            SDL_CreateTextureFromSurface(renderer, frames[currentFrame]);
+        if (texture)
+        {
+            SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+            SDL_DestroyTexture(texture);
+        }
+        SDL_RenderPresent(renderer);
+        file.read(dataInput, sizeof(keyboardEvent));
+        memcpy(&keyboardEvent, dataInput, sizeof(keyboardEvent));
+        if (keyboardEvent.type == EV_KEY && keyboardEvent.value == 1)
+        {
+            if (keyboardEvent.code == KEY_ESC)
+            {
+                running = false;
+            }
+            currentFrame = (currentFrame == 0) ? 3 : 0;
+        }
+        // while (SDL_PollEvent(&event))
+        // {
+        //     if (event.type == SDL_EVENT_QUIT)
+        //     {
+        //         running = false;
+        //     }
+        // }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
+
+    return 0;
 }
